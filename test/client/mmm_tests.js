91c0d6744f628360c4a7827f58e47abede5d327d
Charles Renwick <crenwick@crenwick-mac.local>
2014-12-08 01:04:50 -0800

    changed tests

--- test/client/mmm_tests.js
+++ test/client/mmm_tests.js
@@ -5,7 +5,6 @@ require('angular-mocks');
 
 describe('mmmCtrl', function() {
   var $controllerConstructor;
-  var $httpBackend;
   var $scope;
 
   beforeEach(angular.mock.module('mmmApp'));
@@ -21,22 +20,11 @@ describe('mmmCtrl', function() {
   });
 
   describe('rest request', function() {
-    beforeEach(angular.mock.inject(function(_$httpBackend_) {
-      $httpBackend = _$httpBackend_;
-    }));
 
-    afterEach(function() {
-      $httpBackend.verifyNoOutstandingExpectation();
-      $httpBackend.verifyNoOutstandingRequest();
-    });
-
-    it('make a call to mmm API', function() {
-      $httpBackend.expectPOST('/api/calc')
-        .respond(200, {mean: 2.25, median: 2.5, mode:'3'});
+    it('should get MMM', function() {
       $controllerConstructor('mmmCtrl', {$scope: $scope});
       $scope.user = {input: '1 2 3 3'};
       $scope.update();
-      $httpBackend.flush();
 
       expect($scope.output).toBeDefined();
       expect($scope.output.median).toBe(2.5);
